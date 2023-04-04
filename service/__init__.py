8d2150d2e55b60bcc50bf7d452bb8e0e605b625e
Joseph Stern <josephiswhere@gmail.com>
2023-04-04 01:41:23 +0000

    added to init as per prof recomendation

--- service/__init__.py
+++ service/__init__.py
@@ -17,6 +17,9 @@ app.config.from_object(config)
 # pylint: disable=wrong-import-position, wrong-import-order, cyclic-import
 from service import routes, models  # noqa: E402, E261
 
+# pylint: disable=wrong-import-position
+from service.common import error_handlers, cli_commands  # noqa: F401, E402
+
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
 
